586a4dbc2624a917670eb6d14d478e86d4c805b8
Krittameth Pantharot <krittamethpantarot@gmail.com>
2026-07-27 05:18:25 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.time.LocalDate;
+
+public class Lab05 {
+    public static void main(String[] args) {
+
+        User john = new User("John", 1954, 2, 18);
+        System.out.println(john.getInfo());
+
+        User peter = new User("peter", 2029,3,16);
+        System.out.println(peter.getInfo());
+
+        Admin nicolas = new Admin("Nicolas", 1964, 1, 7);
+        System.out.println(nicolas.getInfo());
+
+        System.out.println(nicolas.getInfo(true));
+        System.out.println(nicolas.getInfo(false));
+
+        // --- Demonstrate isBirthday() / getBirthdayGreeting() ---
+        LocalDate today = LocalDate.now();
+
+        User birthdayUser = new User("Sarah", today.getYear() - 25,
+                today.getMonthValue(), today.getDayOfMonth());
+        Admin birthdayAdmin = new Admin("Marco", today.getYear() - 62,
+                today.getMonthValue(), today.getDayOfMonth());
+
+        System.out.println(birthdayUser.getBirthdayGreeting());
+        System.out.println(birthdayAdmin.getBirthdayGreeting());
+    }
+}
